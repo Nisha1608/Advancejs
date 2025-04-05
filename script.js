c0334acba1ebc7fe0ff310e00d0003e98a470b58
Nisha1608 <you@example.com>
2025-04-06 01:07:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// fuction tester(para1,para2){
+//     return para1+""+para2;
+// }
+// const arg1="argument 1";
+// fuction addtwoNumber(X,y)
+// function addtwoNumber(x,y){
+//     console.log(x+y);
+//     addtwoNumbers();
+//     addtwoNumbers(6,6);
+//     addtwoNumbers(1,2,3,4);
+// }
+//  let spread=["so","much","fun"];
+//  let message=["javascript","is",...spread,"and","very","powerful"];
+
+// function addtwoNumbers(x,y){
+//     console.log(x+y);
+
+// }
+// let arr=[5,9];
+// addtwoNumbers(...arr);
+
+// function addFourNumbers(x,y,z,a){
+//     console.log(x+y+z+a);
+
+// }
+// let arr=[5,9];
+// let arr=[6,7];
+
+// REST PARAMETER
+// function someFunction(param1,...param2){
+//     console.log(param1,param2);
+// }
+// someFunction("hi","there!","How are you?");
+
+// RETURNING FUNCTION VALUES
+// let result=addTwoNumbers(4,5);
+// console.log(result);
+// let resultsArr=[];
+// for(let i=0;i<10;i++){
+//     let result=addTwoNumbers(i,2*i)
+//     resultsArr.push(result);
+// }
+// console.log(resultsArr);
