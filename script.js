770ac25fa5fce95fb28701fde575f11b12828d7e
Nisha1608 <you@example.com>
2025-04-09 14:51:49 +0530

    global var

--- script.js
+++ script.js
@@ -35,9 +35,90 @@
 // RETURNING FUNCTION VALUES
 // let result=addTwoNumbers(4,5);
 // console.log(result);
-// let resultsArr=[];
+
+//  let resultsArr=[];
 // for(let i=0;i<10;i++){
 //     let result=addTwoNumbers(i,2*i)
-//     resultsArr.push(result);
+//      resultsArr.push(result);
+//  }
+//  console.log(resultsArr);
+
+
+// let addtwoNumbers=(x,y)=>x+y;
+// let result=addtwoNumbers(12,15);
+// console.log(result)
+
+// let addTwoNumbers=(x,y)=>{
+//     console.log("Adding...");
+//     return x+y;
 // }
-// console.log(resultsArr);
+
+// variable scope in fuction
+// local variable in fuction
+
+// fuction testAvailability(x){
+//     console.log("Available here:",x);
+// }
+// testAvailability("hi");
+// console.log(" not Available here:",x);
+
+// function testAvailability(){
+//     let y="local variable!";
+//     console.log("Available here:",y);
+// }
+// testAvailability();
+// console.log(" not Available here:",y);
+
+
+// function doingStuff(){
+//     if(true){
+//         var x ="local";
+//     }
+//     console.log(x);
+// }
+// doingStuff();
+
+// function doingStuff(){
+//         if(true){
+//             console.log(x);
+//             var x ="local";
+//         }
+//     }
+        
+//     doingStuff();
+
+    // function doingStuff(){
+    //     if(true){
+    //         console.log(x);
+    //         let x ="local";
+    //     }
+    // }
+        
+    // doingStuff();
+
+    // function doingStuff(){
+    //     if(true){
+            
+    //         const x ="local";
+    //         console.log(x);
+    //     }
+    // }
+        
+    // doingStuff();
+
+
+// global variable
+
+// let globarVar="Accessible everywhere";
+// console.log("Outside function",globarVar);
+
+// function creatingNewScope(x){
+//     console.log("access to global vars inside function",globarVar)
+// }
+// let x="global";
+// function doingStuff(){
+//     let x="local";
+//     console.log();
+// }
+// doingStuff();
+// console.log(x);
