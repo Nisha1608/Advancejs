0822d009e52a3bb11ebcc3384723d1bc38fd0d5c
Nisha1608 <you@example.com>
2025-04-11 13:00:01 +0530

    11/04/2025

--- script.js
+++ script.js
@@ -122,3 +122,128 @@
 // }
 // doingStuff();
 // console.log(x);
+
+
+//immediately invoked function expression
+// (function(){
+//     console.log("IIFE!");
+// })();
+
+
+//arrow func
+// (()=>{
+//     console.log("run right away");
+// })();
+
+//Recursive func
+// function getRecursive(nr){
+//     console.log(nr);
+//     getRecursive(--nr);
+// }
+// getRecursive(3);
+
+
+
+// function getRecursive(nr){
+//     console.log(nr);
+//     if(nr>0){
+//     getRecursive(--nr);
+// }
+// }
+// getRecursive(3);
+
+
+
+
+/* function logRecursive(nr){
+    console.log("Started function",nr);
+    if(nr>0){
+    logRecursive(nr-1);
+}else{
+    console.log("done with recursion");
+}
+console.log("Ended function",nr);
+
+}
+logRecursive(3);
+
+ */
+
+//the performance of recursion is slightly worse than the performance of regular iteration using a loop.So if this cause a bottleneck situation that would really slow down your applica,than you might want to consider another appli
+
+
+//NESTED FUNCTION
+// function doOuterFunctionStuff(nr){
+//     console.log("Outer function");
+//     doInnerFunctionStuff(nr);
+//     function doInnerFunctionStuff(x){
+//         console.log(x+7);
+//         console.log("i can acces outer var",nr);
+//     }
+// }
+// doOuterFunctionStuff();
+
+
+// function doOuterFunctionStuff(nr){
+    
+//     doInnerFunctionStuff(nr);
+//     function doInnerFunctionStuff(x){
+//         let z=10;
+        
+        
+//     }
+//     console.log("i can acces outer var",nr);
+// }
+// doOuterFunctionStuff(2);
+
+//Anonymous function
+
+// function doingStuffAnonymously(){
+//     console.log("not so scret");
+// }
+
+
+//FUNCTION CALLBACKS
+
+
+// function doDlexibleStuff(executeStuff){
+//     executeStuff();
+//     console.log("inside doFlexibleStuffFunction");
+// }
+
+//In javascript there are many built in function ,as you may know by now .One of them is the setTimeout()function .It is a very special function thatbis executing  a certain function after a specified amount of time that it will wait  first.
+
+// let youGotThis=function(){
+//     console.log("You are doing really well ,keep coding");
+
+// };
+// setTimeout(youGotThis,1000);
+
+
+//ques
+// let val=10;
+// function tester(val){
+//     val +=10;
+//     if(val<100){
+//         return tester(val);
+//     }
+//     return val;
+// }
+// tester(val);
+// console.log(val);
+
+// ques
+// let testFunction=function(){
+//     console.log("hello")
+// }();
+
+//ques
+// let test2=(num)=>num+5;
+// console.log(test2(14));
+
+// ques
+// var addFive1=function addFive1(num){
+//     return num +2;
+// };
+// let addFive2=(num)=>num+2;
+// console.log(addFive1(14));
